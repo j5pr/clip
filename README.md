e36d9a339f50bed1b90c8ed62f13576b0ad8b11f
Brian Shao <brian@jzshao.com>
2024-03-28 13:39:29 -0700

    feat: it works

---
+++ README.md
@@ -0,0 +1,1 @@
+for when youre trying to optimize your os so much you dont even have a clipboard
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+use std::io::{Read, Write};
+use clap::Parser;
+use arboard::Clipboard;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Write stdin to clipboard
+    #[arg(short, long)]
+    write: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let mut clip = Clipboard::new().unwrap();
+
+    if args.write {
+        let mut buf = vec![];
+        io::stdin().read_to_end(&mut buf).unwrap();
+        io::stdout().write(&buf).unwrap();
+        clip.set_text(String::from_utf8(buf).unwrap()).unwrap();
+    } else {
+        io::stdout().write(clip.get_text().unwrap().as_bytes()).unwrap();
+    }
 }
