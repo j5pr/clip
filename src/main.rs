e6c6048a7d171a5074ef2f240845cf8597949d1a
Brian Shao <brian@jzshao.com>
2024-03-29 20:57:13 -0700

    feat: error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use arboard::Clipboard;
+use clap::Parser;
 use std::io;
 use std::io::{Read, Write};
-use clap::Parser;
-use arboard::Clipboard;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -9,18 +9,89 @@ struct Args {
     /// Write stdin to clipboard
     #[arg(short, long)]
     write: bool,
+
+    /// Log additional error details
+    #[arg(short, long)]
+    verbose: bool,
+}
+
+enum Error {
+    ClipboardError(arboard::Error),
+    ConversionError(std::string::FromUtf8Error),
+    IoError(std::io::Error),
 }
 
 fn main() {
     let args = Args::parse();
     let mut clip = Clipboard::new().unwrap();
 
-    if args.write {
-        let mut buf = vec![];
-        io::stdin().read_to_end(&mut buf).unwrap();
-        io::stdout().write(&buf).unwrap();
-        clip.set_text(String::from_utf8(buf).unwrap()).unwrap();
+    let result = if args.write {
+        try_write(&mut clip)
     } else {
-        io::stdout().write(clip.get_text().unwrap().as_bytes()).unwrap();
+        try_read(&mut clip)
+    };
+
+    if let Err(e) = result {
+        handle_error(e, args.verbose);
     }
+}
+
+fn try_read(clip: &mut Clipboard) -> Result<(), Error> {
+    let text = clip.get_text().map_err(|e| Error::ClipboardError(e))?;
+
+    io::stdout()
+        .write(&text.as_bytes())
+        .map(|_| ())
+        .map_err(|e| Error::IoError(e))
+}
+
+fn try_write(clip: &mut Clipboard) -> Result<(), Error> {
+    let mut buf = Vec::new();
+
+    io::stdin()
+        .read_to_end(&mut buf)
+        .map_err(|e| Error::IoError(e))?;
+
+    let str = String::from_utf8(buf).map_err(|e| Error::ConversionError(e))?;
+
+    clip.set_text(&str).map_err(|e| Error::ClipboardError(e))?;
+
+    io::stdout()
+        .write(&str.as_bytes())
+        .map(|_| ())
+        .map_err(|e| Error::IoError(e))
+}
+
+fn handle_error(e: Error, verbose: bool) {
+    let msg = if !verbose {
+        match e {
+            Error::ClipboardError(_) => "clipboard error",
+            Error::ConversionError(_) => "utf8 conversion error",
+            Error::IoError(_) => "io error",
+        }
+    } else {
+        match e {
+            Error::ClipboardError(err) => match err {
+                arboard::Error::ClipboardOccupied => "clipboard not available",
+                arboard::Error::ContentNotAvailable => "content not available",
+                arboard::Error::ConversionFailure => "could not convert clipboard content",
+                arboard::Error::ClipboardNotSupported => "clipboard not supported",
+                arboard::Error::Unknown { description } => {
+                    eprintln!("clipboard error: {}", description);
+                    return;
+                }
+                _ => "unknown clipboard error",
+            },
+            Error::IoError(err) => {
+                eprintln!("io error: {}", err);
+                return;
+            }
+            Error::ConversionError(err) => {
+                eprintln!("utf8 error: {}", err);
+                return;
+            }
+        }
+    };
+
+    eprintln!("error: {}", msg);
 }
